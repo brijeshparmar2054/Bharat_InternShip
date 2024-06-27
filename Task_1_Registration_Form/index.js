315f663c93d9f3099c0cc9fa0db8eaf612df0cc7
Brijesh Parmar <parmarbrijesh2054@gmail.com>
2024-06-27 13:50:54 +0530

    Updated All Project Tasks

--- Task_1_Registration_Form/index.js
+++ Task_1_Registration_Form/index.js
@@ -12,27 +12,27 @@ app.use(express.static(__dirname + '/public'));
 mongoose.connect('mongodb://localhost:27017/Database');
 var db = mongoose.connection;
 
-db.on('error', ()  => console.log('---FAILED to connect to Database'));
+db.on('error', () => console.log('---FAILED to connect to Database'));
 db.once('open', () => console.log('+++Connected to Database'));
 
 
-app.post("/sign_up",(req,res)=>{
+app.post("/sign_up", (req, res) => {
     var name = req.body.name;
     var age = req.body.age;
-    var email =req.body.email;
+    var email = req.body.email;
     var phone = req.body.phno;
     var gender = req.body.gender;
     var password = req.body.password;
     var data = {
-        "name":name,
-        "age":age,
-        "email":email,
-        "phone":phone,
-        "gender":gender,
-        "password":password,
+        "name": name,
+        "age": age,
+        "email": email,
+        "phone": phone,
+        "gender": gender,
+        "password": password,
     }
-    db.collection('users').insertOne(data,(err,collection)=>{
-        if(err){
+    db.collection('users').insertOne(data, (err, collection) => {
+        if (err) {
             throw err;
         }
         console.log("Record Inserted Successfully");
